06e65da0f40cc19ecc2de79e1f1e4bfd8683ea7b
Slye-Man <selaelosebola67@gmail.com>
2025-12-15 04:13:03 +0200

    �: Initialized database with EF Core
    🔧: Initialized database with EF Core

---
+++ Domain/Migrations/20251027172856_CartDescription.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class CartDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Carts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Carts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Carts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Carts");
+        }
+    }
+}
