730d532c1d2e69fb770037776f9386f7f99274ff
Slye-Man <selaelosebola67@gmail.com>
2025-12-19 02:28:54 +0200

    🔧: Minor adjustment to AuthResponseDTO file

--- Api/DTO/AuthResponseDTO.cs
+++ Api/DTO/AuthResponseDTO.cs
@@ -3,7 +3,7 @@ namespace Api.DTO;
 public class AuthResponseDTO
 {
     public int UserId { get; set; }
-    public string FirstName { get; set; }
     public string UserName { get; set; }
     public string Email { get; set; }
+    public string? SessionId { get; set; }
 }
