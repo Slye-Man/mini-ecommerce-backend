25aab6a08b2423d385f614c147342c9b50db97e0
Slye-Man <selaelosebola67@gmail.com>
2025-12-22 03:22:35 +0200

    🔧: Fixed non-null type for Description variable

--- Domain/Carts/Cart.cs
+++ Domain/Carts/Cart.cs
@@ -12,7 +12,7 @@ public class Cart
     [Required]
     [ForeignKey(nameof(User))]
     public int UserId { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 
